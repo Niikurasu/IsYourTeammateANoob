b18c9f4ff8175587ad124caacad5d5d662eb1a2f
Unknown <niklas.eriksson@fgz.ch>
2018-05-24 20:10:00 +0200

    js

--- scripts/api-request.js
+++ scripts/api-request.js
@@ -2,34 +2,4 @@
 
 // API Key -> 78d9cdf2-5e59-433f-9116-05dbd02e6564
 
-function MyController($scope, $http) {
-    var platform = "pc";
-    var name = 'Nikurasu.'
-        $http.get('https://api.fortnitetracker.com/v1/profile/' + platform + '/' + name + '', {
-            headers: {'TRN-Api-Key': '78d9cdf2-5e59-433f-9116-05dbd02e6564'}
-        });
-
- }
-
-function httpGet(theUrl, key)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.setRequestHeader("TRN-Api-Key:", key);
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-function getStats(platform, name)
-{
-    //console.log('https://api.fortnitetracker.com/v1/profile/' + platform + '/' + name + '')
-   // httpGet('https://api.fortnitetracker.com/v1/profile/' + platform + '/' + name + '', '78d9cdf2-5e59-433f-9116-05dbd02e6564')
-   return send(platform, name)
-}
-
-function send(platform, name) 
-{
-    $http.get('https://api.fortnitetracker.com/v1/profile/' + platform + '/' + name + '', {
-        headers: {'TRN-Api-Key': '78d9cdf2-5e59-433f-9116-05dbd02e6564'}
-    });
-}
+var key = '78d9cdf2-5e59-433f-9116-05dbd02e6564';
